af0484dfeb0001b9b5d0e1f14184d1bd5afeb04a
thanatat64 <thanatat64@gmail.com>
2024-02-19 23:40:33 +0700

    test ja

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/getcode")
 async def root():
-    return {"message": "Softdev Inpractice gametuatueng1"}
+    return {"message": "Softdev Inpractice gametuatueng long eng ja"}
 
 @app.get("/plus/{num1}/{num2}")
 async def root(num1: int, num2: int):
