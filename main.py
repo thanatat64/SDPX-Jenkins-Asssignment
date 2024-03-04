68a5c4ac6c1c8d8bb32d66b97257ec9913bdd9a3
thanatat64 <thanatat64@gmail.com>
2024-03-05 03:08:11 +0700

    prepare for Final Exam

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ app = FastAPI()
 async def root():
     return {"message": "test final exam"}
 
+@app.get("/is_prime/{x}")
+async def prime(x:int):
+    if x < 2:
+        return False
+    for i in range(2, int(x**0.5)+1):
+        if x % i ==0:
+            return False
+    return True
+
+
 @app.get("/plus/{num1}/{num2}")
 async def root(num1: int, num2: int):
     result = num1 + num2
--- unit_test.py
+++ unit_test.py
@@ -1,18 +1,35 @@
 import unittest
-from fastapi.testclient import TestClient
 from main import app
+from fastapi.testclient import TestClient
 
 class AppTestCase(unittest.TestCase):
     def setUp(self):
         self.client = TestClient(app)
-    def test_plus_endpoint(self):
+        
+    def test_true_when_x_is_17 (self):
+        response = self.client.get("is_prime/17")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.json)
+
+    def test_false_when_x_is_36(self):
+        response = self.client.get("/is_prime/36")
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.json())
+
+    def test_true_when_x_is_13219(self):
+        response = self.client.get("is_prime/13219")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.json())
+   
+    def test_plus_5_6(self):
         response = self.client.get("/plus/5/6")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {"result": 11})
-
+        
+    def test_plus_8_8(self):
         response = self.client.get("/plus/8/8")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {"result": 16})
-
+        
 if __name__ == "__main__":
     unittest.main()
